2c7c75c333734c8df74faa5073096b761b7eb585
Jake Priddy <priddyjacob84@gmail.com>
2016-05-30 10:41:20 -0400

    added main page styling/ added init firebase

---
+++ src/scripts/app.js
@@ -0,0 +1,8 @@
+//Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB7DQENoTDJb8T1Fu-QHl6ZCmo-gwLgz0g",
+    authDomain: "testfire-62669.firebaseapp.com",
+    databaseURL: "https://testfire-62669.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config);
