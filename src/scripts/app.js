97fdaceb389fd3e487ea4c65d26a08ba1c39b592
Jake Priddy <priddyjacob84@gmail.com>
2016-05-31 18:13:37 -0400

    5/31 1813

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,4 +5,18 @@ var config = {
     databaseURL: "https://testfire-62669.firebaseio.com",
     storageBucket: "",
   };
-  firebase.initializeApp(config);
+//get firebase app and all primitives used
+var app = firebase.initializeApp(config);
+var database = app.database();
+var auth = app.auth();
+var storage = app.storage();
+
+//get a refrence to our "chat room" in the database
+var databaseRef = database.ref().child('chat');
+
+//Handles whan you click "Send" button
+btUpdateMessage.addEventListener('click', function(e) {
+    var chat = { name: username, message: txtNewMessage.value };
+    addMessage(chat);
+    txtNewMessage.value = '';
+});
