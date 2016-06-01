6dea738a0a2a8d32f9d209474945956734bb5728
Jake Priddy <priddyjacob84@gmail.com>
2016-06-01 13:28:06 -0400

    6/1 1327

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -14,12 +14,18 @@ var storage = app.storage();
 //get a refrence to our "chat room" in the database
 var databaseRef = database.ref().child('chat');
 //get varibales from input box
-var message = document.getElementById('txtNewMessage');
-//when you click the "update" button 
-document.getElementById('btUpdateMessage').addEventListener('click', function(e) {
+var message = document.getElementById('txtNewMessage').value();
+//when you click the "update" button...
+var update = document.getElementById('btUpdateMessage');
+
+update.addEventListener('click', function(e) {
+    //defaults from google io 2016
+    //build new data input
 //    var chat = { name: username, message: message.value };
+    //add new data input to the board
 //    addMessage(chat);
+    //clear input box to bring back to original state
 //    message.value = '';
-    //console log the message string
-   return console.log(message);
+    //console log the message string 
+  return console.log(message);
 });
