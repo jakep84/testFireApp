0eb7f3ef01865849464728535ed0b712b2870631
Jake Priddy <priddyjacob84@gmail.com>
2016-05-31 23:52:19 -0400

    5/31 2352

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,9 @@
 //Initialize Firebase
 var config = {
-    apiKey: "AIzaSyB7DQENoTDJb8T1Fu-QHl6ZCmo-gwLgz0g",
-    authDomain: "testfire-62669.firebaseapp.com",
-    databaseURL: "https://testfire-62669.firebaseio.com",
-    storageBucket: "",
+    apiKey: 'AIzaSyB7DQENoTDJb8T1Fu-QHl6ZCmo-gwLgz0g',
+    authDomain: 'testfire-62669.firebaseapp.com',
+    databaseURL: 'https://testfire-62669.firebaseio.com',
+    storageBucket: 'testfire-62669.appspot.com'
   };
 //get firebase app and all primitives used
 var app = firebase.initializeApp(config);
@@ -13,10 +13,13 @@ var storage = app.storage();
 
 //get a refrence to our "chat room" in the database
 var databaseRef = database.ref().child('chat');
-
-//Handles whan you click "Send" button
-btUpdateMessage.addEventListener('click', function(e) {
-    var chat = { name: username, message: txtNewMessage.value };
-    addMessage(chat);
-    txtNewMessage.value = '';
+//get varibales from input box
+var message = document.getElementById('txtNewMessage');
+//when you click the "update" button 
+document.getElementById('btUpdateMessage').addEventListener('click', function(e) {
+//    var chat = { name: username, message: message.value };
+//    addMessage(chat);
+//    message.value = '';
+    //console log the message string
+   return console.log(message);
 });
